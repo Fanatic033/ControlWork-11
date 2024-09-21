7a23de68271db934a14fc5eaa97a4eae216dc3c2
ilzat <ilzat.sadykov.2006@bk.ru>
2024-09-21 16:11:49 +0600

    "сделал подключения tinymce"

--- frontend/src/features/Products/ProductsThunks.ts
+++ frontend/src/features/Products/ProductsThunks.ts
@@ -1,6 +1,8 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import axiosApi from "../../axiosApi.ts";
-import {ProductMutation, Products} from "../../types.ts";
+import {GlobalError, ProductMutation, Products} from "../../types.ts";
+import {RootState} from "../../app/store.ts";
+import {isAxiosError} from "axios";
 
 export const fetchProducts = createAsyncThunk<Products[], string | undefined>(
     'products/fetchAll',
@@ -10,18 +12,35 @@ export const fetchProducts = createAsyncThunk<Products[], string | undefined>(
     },
 );
 
-export const createProduct = createAsyncThunk<void, ProductMutation>('products/create', async (productMutation) => {
-    const formData = new FormData();
+export const createProduct = createAsyncThunk<void, ProductMutation, {
+    rejectValue: GlobalError, state: RootState
+}>('products/create', async (productMutation, {rejectWithValue, getState}) => {
+    try {
+        const formData = new FormData();
 
-    const keys = Object.keys(productMutation) as (keyof ProductMutation)[];
-    keys.forEach((key) => {
-        const value = productMutation[key];
-        if (value !== null) {
-            formData.append(key, value);
+        const keys = Object.keys(productMutation) as (keyof ProductMutation)[];
+        keys.forEach((key) => {
+            const value = productMutation[key];
+            if (value !== null) {
+                formData.append(key, value);
+            }
+        });
+
+        const token = getState().users.user?.token;
+        if (!token) {
+            console.error('No user token found');
         }
-    });
-
-    await axiosApi.post('/products', formData);
+        await axiosApi.post('/products', formData, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (e) {
+        if (isAxiosError(e) && e.response && e.response.status === 400) {
+            return rejectWithValue(e.response.data)
+        }
+        throw e
+    }
 });
 
 export const fetchOneProduct = createAsyncThunk<Products, string>('products/fetchOne', async (id) => {
